0e18d911cd007a348fabffd2a2fe9b4120cb5823
jasmine liang <jasmine.liang@walmart.com>
2025-03-23 00:39:23 -0700

    live demo

--- src/declarations.ts
+++
@@ -1,6 +0,0 @@
-
-//Basic declarations
-
-export const hubDataUrl = 'https://d1q0vy0v52gyjr.cloudfront.net/hub.json';
-
-export const collectionByIdUrl = 'https://d1q0vy0v52gyjr.cloudfront.net/collections/<id>.json';
